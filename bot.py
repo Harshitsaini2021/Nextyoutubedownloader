25baf2558672f7952cde6ad71acb91472ee71a57
Harshitsaini2021 <78420021+Harshitsaini2021@users.noreply.github.com>
2022-04-03 13:50:51 +0530

    Update
    @server.route('/' + TOKEN, methods=['POST'])
    def getMessage():
       bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
       return "!", 200
    @server.route("/")
    def webhook():
       bot.remove_webhook()
       bot.set_webhook(url='https://nextyoutube.herokuapp.com/' + TOKEN)
       return "!", 200
    if __name__ == "__main__":
       server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from pytube import YouTube
 from pytube.cli import on_progress
 import urllib.request
 
-from flask import Flask,request
 import os
 
 
@@ -12,7 +11,7 @@ TOKEN = "5169425026:AAHAosbSQ36rHwjqyjUigefoWejFmvQyRro"
 
 bot = telebot.TeleBot(token=TOKEN, parse_mode=None)
 
-server = Flask(__name__)
+
 
 def makeMarkup(arr):
 	markup = types.InlineKeyboardMarkup()
@@ -59,17 +58,6 @@ def download(message):
 bot.infinity_polling()
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-   return "!", 200
-@server.route("/")
-def webhook():
-   bot.remove_webhook()
-   bot.set_webhook(url='https://nextyoutube.herokuapp.com/' + TOKEN)
-   return "!", 200
-if __name__ == "__main__":
-   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-   
+
    
    

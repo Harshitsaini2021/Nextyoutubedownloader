b50c6fab293b8afcc7a45440f79cad28905fb04c
harshitsaini2021 <harshitsainiboy@18.gmail.com>
2022-04-03 12:11:28 +0530

    make it better

---
+++ bot.py
@@ -0,0 +1,74 @@
+import telebot
+from telebot import types
+from pytube import YouTube  
+from pytube.cli import on_progress
+import urllib.request
+
+from flask import Flask,request
+import os
+
+
+TOKEN = "5169425026:AAFQ6gYDv1jtus96UGfbwko5vl2mt1mo_aA"
+
+bot = telebot.TeleBot(token=TOKEN, parse_mode=None)
+
+server = Flask(__name__)
+
+def makeMarkup(arr):
+	markup = types.InlineKeyboardMarkup()
+	for key,value in arr.items():
+		markup.add(types.InlineKeyboardButton(text=value,callback_data=key))
+	return markup
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+	bot.reply_to(message, "Your most welcome on me. I am best YouTube videos downloader bot 😎. You can download any valid video by using me.")
+
+
+@bot.callback_query_handler(func=lambda call:True)
+def get(call):
+	itag = call.data
+	bot.edit_message_text(chat_id=call.message.chat.id,text='Yes, I am downloading file...', message_id=call.message.message_id)
+	#yt.streams.get_by_itag(itag=itag).download()
+	url = yt.streams.get_by_itag(itag=itag).url
+	data = urllib.request.urlopen(url).read()
+	bot.send_video(caption=yt.title, reply_to_message_id=call.message.chat.id, thumb=yt.thumbnail_url, chat_id=call.message.chat.id,video=data,timeout=10000)
+	bot.edit_message_text(chat_id=call.message.chat.id,text='Download complete', message_id=call.message.message_id)
+	
+@bot.message_handler(func=lambda m: True)
+def download(message):
+	chat_id = message.chat.id
+	link = message.text
+	try:
+	    global yt
+	    yt = YouTube(link,on_progress_callback=on_progress)  
+	    print('Downloading...')
+	    bot.send_message(chat_id=chat_id,text=f'Okk, I am downloading "{yt.title}" on my server')
+	    stream = yt.streams.filter(type='video',progressive=True).order_by('resolution')
+	    videos = {}
+	    for i in stream:
+	    	videos[i.itag]=str(str(i.mime_type).split('/')[1]+','+','+ i.resolution+','+ str(round(i.filesize_approx/1048576,2))+'MB') 
+	    
+	    bot.send_message(chat_id=chat_id,text='Downloaded 😊',reply_markup=makeMarkup(videos),parse_mode='HTML')
+	except Exception as e:
+		print(e,"Connection Error") 
+		bot.send_message(chat_id=chat_id,text="Sorry I can't recognize you , you said '%s'" % message.text) 
+
+bot.infinity_polling()
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+   return "!", 200
+@server.route("/")
+def webhook():
+   bot.remove_webhook()
+   bot.set_webhook(url='https://nextyoutube.herokuapp.com/' + TOKEN)
+   return "!", 200
+if __name__ == "__main__":
+   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+   
+   
+   
